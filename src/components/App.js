41ee94dd2d3f3afc77c32dd1f1ddbc9a35edebf5
Vivek Tiwari <79350756+tivivek@users.noreply.github.com>
2023-01-16 00:39:16 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,29 @@
 import React, {Component, useState} from "react";
-import "./../styles/App.css";
+import '../styles/App.css';
 
-function App() {
-  return (
-    <div id="main">
-      // Do not alter the main div
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+		super(props);
+		this.state ={
+			render: false
+		}
+		this.clickHandle = this.clickHandle.bind(this);
+	};
+
+	clickHandle(){
+		this.setState({render : !this.state.render});
+	}
+
+    render() {
+    	return(
+    		<div id="main">
+				{ /* Do not remove this main div!! */ }
+				{this.state.render && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
+				<button id="click" type="button" onClick={this.clickHandle}>Click</button>
+    		</div>
+    	);
+    }
 }
-
 
 export default App;
